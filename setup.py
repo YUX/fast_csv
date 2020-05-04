0451c5c0d649b38bc3843af3f60e9951b14406b7
Yu XIAO <yu.xiao.fr@gmail.com>
2020-05-04 23:25:42 +0800

    CI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open(("README.md"), encoding='utf-8') as f:
 
 setup(
     name='fast_csv',
-    version='1.2.1',
+    version='1.2.2',
     packages=find_packages(),
     url='https://github.com/YUX-IO/fast_csv',
     license='MIT',
